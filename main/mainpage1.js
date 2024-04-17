314cae202994303b2fbe5e7292a9485877c8de28
seungyeon <lsy1164@naver.com>
2024-04-18 04:15:36 +0900

    refactor:

---
+++ main/mainpage1.js
@@ -0,0 +1,43 @@
+const timer = document.querySelector(".js-timer");
+const toggleBtn = document.querySelector("recording_button");
+
+let TIME = 0;
+let cron; // setInterval을 위한 변수
+let isTimerRunning = false;
+
+function toggleTimer() {
+  if (isTimerRunning) {
+    stopTimer();
+  } else {
+    startTimer();
+  }
+}
+
+function startTimer() {
+  updateTimer();
+  cron = setInterval(updateTimer, 1000);
+  isTimerRunning = true;
+  toggleBtn.innerText = "Stop Timer";
+  timer.classList.add("hide");
+}
+
+function stopTimer() {
+  clearInterval(cron);
+  isTimerRunning = false;
+  toggleBtn.innerText = "Start Timer";
+  timer.classList.remove("hide");
+}
+
+function updateTimer() {
+  const hours = Math.floor(TIME / 3600);
+  const checkMinutes = Math.floor(TIME / 60);
+  const seconds = TIME % 60;
+  const minutes = checkMinutes % 60;
+
+  timer.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  TIME++;
+}
+
+toggleBtn.addEventListener("click", toggleTimer);
