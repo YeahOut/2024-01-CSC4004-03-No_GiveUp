6c342bb1194a1d26b70347e7101cef3931322055
J1miin <gift040404@naver.com>
2024-05-05 00:29:40 +0900

    :sparkles: signup1,2 html -> template 변경
    - signup1,2 html -> template 문법으로 변경
    - librosa 작동 test (아직 정리 안됨)

---
+++ MyV/signup/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('1/', views.signup1, name='signup1'),
+    path('2/', views.signup2, name='signup2'),
+    path('3/', views.signup2, name='signup3'),
+]
---
+++ MyV/signup/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def signup1(request):
+    return render(request, 'signup/signup1.html')
+
+def signup2(request):
+    return render(request, 'signup/signup2.html')
