c449b925da1bce74a8290340e7fef2489e3deb25
J1miin <gift040404@naver.com>
2024-05-17 13:07:28 +0900

    :sparkles: Admin 페이지에 음원파일 모델 생성

--- MyV/modal/models.py
+++ MyV/modal/models.py
@@ -3,3 +3,5 @@ from django.db import models
 class UploadAnalyzeFile(models.Model):
     mySound_file = models.FileField(upload_to='vocalReportSource/')
     compareSound_file = models.FileField(upload_to='vocalReportSource/')
+    fMySound_name = models.CharField(max_length=255, default='default')
+    fCompareSound_name = models.CharField(max_length=255, default='default')
