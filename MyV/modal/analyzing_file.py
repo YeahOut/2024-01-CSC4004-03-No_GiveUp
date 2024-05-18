b44477297997f78304fc899ddda25af7301470e8
J1miin <gift040404@naver.com>
2024-05-18 23:36:11 +0900

    :chart_with_upwards_trend: 수정된 보컬분석 장고에 연결 완료
    하드코딩 된 부분이 많지만.. 우선 기능 구현은 확인 완료

--- MyV/modal/analyzing_file.py
+++ MyV/modal/analyzing_file.py
@@ -30,40 +30,42 @@ def downloadFile() :
 
     #파일 다운로드 진행하기
     mine_obj_file= 'vocalReportSource/usr.wav' #디렉토리 버킷 접근하기
-    compare_obj_file='vocalReportSource/org.wav'
+    compare_obj_file='vocalReportSource/org.m4a'
     print("###버킷 접근 성공###")
     
     save_file = os.path.join(os.getcwd(), 'media', 'vocalReportSrc', 'usr.wav') #저장위치 및 파일 다른 이름으로 저장하기 but 이름변경 안할거임
     bucket.download_file(mine_obj_file,save_file)
 
-    save_file = os.path.join(os.getcwd(), 'media', 'vocalReportSrc', 'org.wav') #저장위치 및 파일 다른 이름으로 저장하기 but 이름변경 안할거임
+    save_file = os.path.join(os.getcwd(), 'media', 'vocalReportSrc', 'org.m4a') #저장위치 및 파일 다른 이름으로 저장하기 but 이름변경 안할거임
     bucket.download_file(compare_obj_file,save_file)
     return 1;
 
 def process_file():
     # 반주 & 보컬 분리
-    #spleet(org)
+    org = "org"
+    usr = "usr"
+
+    #확장자 예외처리
+    org_convert_format()
+    print("##확장자 예외처리 성공##")
+    #usr_convert_format()
+
+    # 반주 & 보컬 분리
+    spleet(org)
     #spleet(usr)
-    separator = Separator('spleeter:5stems')
-    #분리할 음원 파일 경로 만들기 
-    audio_file = os.getcwd()+"/media/vocalReportSrc/kaze_younha_sliced.mp3"
-    #분리할 음원의 경로를 전달해서, 음원 분리하고, 현재 디렉토리 (os.getcwd())에 분리한 음원 파일 폴더 생성하기 
-    #이 폴더는 추후에 다 사용한 후에는 삭제가 되어야 한다. --> os.remove로 처리할 부분
-    separator.separate_to_file(audio_file, os.getcwd())
-
-    #os.getcwd()가 os.path.join base_dir와 동일한 역할
-    org = os.path.join(settings.BASE_DIR, "kaze_younha_sliced/vocals")
-    #->"kaze_younha_sliced/vocals" 이게 sperator로 분리해서 만들어진 폴더/vocals(자체 생성하는 보컬파일 이름)의 경로 (확장자 제외)
-    usr = os.path.join(settings.BASE_DIR, "media/vocalReportSrc", "kaze_mine")
-
+    
     plt.figure(figsize=(12, 4))
     print("#################분리완#################")
-    org_name = org + ".wav" #librosa에서 돌릴 음원파일의 경로 (즉 분리된 음원 이름까지 경로를 가져오고, wav를 붙여서 확장자 변경)
-    usr_name = usr + ".wav"
+    org_name = os.path.join(os.getcwd(),'media', 'vocalReportSrc', "org.wav") #librosa에서 돌릴 음원파일의 경로 (즉 분리된 음원 이름까지 경로를 가져오고, wav를 붙여서 확장자 변경)
+    usr_name = os.path.join(os.getcwd(),'media', 'vocalReportSrc', "usr.wav") #librosa에서 돌릴 음원파일의 경로 (즉 분리된 음원 이름까지 경로를 가져오고, wav를 붙여서 확장자 변경)
     
     ##리브로사 분석 시작##
     y_org, sr_org = librosa.load(org_name)
     y_usr, sr_usr = librosa.load(usr_name)
+
+    print(f"org 업로드 확인하기: {len(y_org)} samples at {sr_org} Hz")
+    print(f"usr 업로드 확인하기: {len(y_usr)} samples at {sr_usr} Hz")
+
     f0_org, voiced_flag_org, voiced_prob_org = librosa.pyin(y=y_org, fmin=60, fmax=2000, sr=sr_org)
     f0_usr, voiced_flag_usr, voiced_prob_usr = librosa.pyin(y=y_usr, fmin=60, fmax=2000, sr=sr_usr)
     frames_org = range(len(f0_org))
@@ -81,16 +83,16 @@ def process_file():
 
     f1_org = f0_org.copy()
     f1_usr = f0_usr.copy()
-
+    print("###test1###")
     t_usr, t0, idx = get_start_pos(f1_org=f0_org, f1_usr=f0_usr, t_org=t_org, t_usr=t_usr)
-
-    score, min_note, max_note, best_idx = accuracy_analysis(t_usr, idx, f0_org, f0_usr)
+    print("###test###")
+    score, min_note, max_note, best_idx = accuracy_analysis(t_org, t_usr, idx, f0_org, f0_usr)
+    print("###test###")
     best_st = t_usr[best_idx]
     best_ed = t_usr[best_idx + len(f0_usr) // 5]
 
-    print("#################제거완료#################")
-    #os.remove(os.getcwd()+'/kaze_younha_sliced')
-
+    remove_prefiles()
+    
     print("사용자가 부른 부분은 음원의 {}초부터 입니다".format(int(t0)))
     print("점수 :", int(score * 100))
     print("최고음 :", max_note)
@@ -100,22 +102,34 @@ def process_file():
     return max_note, min_note, int(t0), best_st, best_ed
 
 def spleet(org_file_name):
-    if (os.path.isfile("./output")):
-        shutil.rmtree("./output")
+    output_dir = os.path.join(os.getcwd(), 'output') #output폴더를 생성할 경로 정해주기
+    if (os.path.isfile(output_dir)):
+        shutil.rmtree(output_dir)
+
+    #os.makedirs(output_dir)#spleet 함수 쓸 경우 만들어야 하니까
     stems = 5
-    file_name = org_file_name
-
-    spl = r'spleeter separate -p spleeter:' + \
-          str(stems) + r'stems -o output ' + file_name + '.mp3'
+    file_path = os.path.join(os.getcwd(),'media','vocalReportSrc',org_file_name) #mp3파일이 있는 폴더로 파일명 정해주기 (확장자는 not yet) 
+    file_name = org_file_name #그냥 파일명 org, usr 이렇게 
+    spl = f'spleeter separate -p spleeter:{stems}stems -o "{output_dir}" "{file_path}.m4a"'
     os.system(spl)
+    print("##check point1##")
 
-    src = os.path.join(settings.BASE_DIR, 'modal/output') + file_name + "/vocals.wav"
-    dst = os.path.join(settings.BASE_DIR, 'modal')
-    shutil.copy2(src, dst)
-    shutil.rmtree(os.path.join(settings.BASE_DIR, 'modal/output'))
+    src = os.path.join(output_dir, file_name, "vocals.wav") #spleet 함수로 만든 vocal.wav 찾기
+    dst = os.path.join(os.getcwd(), 'media', 'vocalReportSrc') #spleet 함수가 만든 vocal.wav 파일을 meida/vocalReportSrc로 옮기기 위한 도착 경로
+
+    print("##check point2##")
+    print("src: ", src) #for debug
+    print("dst: ", dst) #for debug
+    
+    shutil.copy2(src, dst) #src voacl.wav copy to dst path
+    print("##check point3##")
+
+    shutil.rmtree(output_dir) #spleet 함수로 만든 Output 폴더 삭제하기
+    print("##rmtree check point4##")
+
     if (os.path.isfile(file_name + ".wav")):
         os.remove(file_name + ".wav")
-    os.rename("vocals.wav", file_name + ".wav")
+    os.rename(os.path.join(dst,"vocals.wav"), os.path.join(dst,"org.wav"))
 
 def get_start_pos(f1_org, f1_usr, t_org, t_usr):
     # 두 함수 미분
@@ -140,7 +154,7 @@ def get_start_pos(f1_org, f1_usr, t_org, t_usr):
     return t_usr, t0, max_idx
 
 
-def accuracy_analysis(t_usr, idx, f0_org, f0_usr):
+def accuracy_analysis(t_org, t_usr, idx, f0_org, f0_usr):
     track_len = len(f0_usr)
     f0_org = f0_org[idx:idx + track_len]
 
@@ -194,28 +208,28 @@ def accuracy_analysis(t_usr, idx, f0_org, f0_usr):
                   opacity=0.25, line_width=0, label=dict(text="가장 정확한 구간"))
     fig.update_layout(legend_yanchor='top', legend_y=0.99, legend_xanchor='left', legend_x=0.01,
                       margin_l=0, margin_r=0, margin_b=0, margin_t=0)
-    fig.write_image('./vocal_report_graph.png')
-    fig.show()
+    #fig.write_image('./vocal_report_graph.png')
+    #fig.show()
     return score, min_note, max_note, best_idx
 
 ##0517 update
 def org_convert_format():
     is_org_m4a = False
-    if (os.path.isfile("./org.m4a")):
+    if (os.path.isfile(os.path.join(os.getcwd(), 'media', 'org.m4a'))) :
         is_org_m4a = True
 
-    if(~is_org_m4a):
-        org_audio = AudioSegment.from_file("./org.m4a", format="m4a")
-        org_audio.export("./org.mp3", format="mp3")
+    if(is_org_m4a):
+        org_audio = AudioSegment.from_file(os.path.join(os.getcwd(), 'media','org.m4a'), format="m4a")
+        org_audio.export(os.path.join(os.getcwd(), 'media', 'org.mp3'), format="mp3")
 
 def usr_convert_format():
     is_usr_m4a = False
-    if (os.path.isfile("./usr.m4a")):
+    if (os.path.isfile(os.path.join(os.getcwd(), 'media','usr.m4a'))):
         is_usr_m4a = True
 
-    if(~is_usr_m4a):
-        org_audio = AudioSegment.from_file("./usr.m4a", format="m4a")
-        org_audio.export("./usr.mp3", format="mp3")
+    if(is_usr_m4a):
+        org_audio = AudioSegment.from_file(os.path.join(os.getcwd(), 'media', 'usr.m4a'), format="m4a")
+        org_audio.export(os.path.join(os.getcwd(), 'media','usr.mp3'), format="mp3")
 
 def remove_prefiles():
     if (os.path.isfile("org.wav")):
--- MyV/modal/models.py
+++ MyV/modal/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class UploadAnalyzeFile(models.Model):
+    # 기존의 한 사용자로부터 인스턴스가 두 개씩 생기는 문제 방지 by setting user id as FK 
+    #user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # AWS/vocalReportSource 디렉토리 버킷에 파일 업로드 
     mySound_file = models.FileField(upload_to='vocalReportSource/')
     compareSound_file = models.FileField(upload_to='vocalReportSource/')
+    # 업로드한 파일명 저장
     fMySound_name = models.CharField(max_length=255, default='default')
     fCompareSound_name = models.CharField(max_length=255, default='default')
