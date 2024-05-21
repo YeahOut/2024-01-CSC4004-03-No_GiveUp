60586e75a35a751b1c5d8179d1b0e9133ebd19a3
YeahOut <pt840072@gmail.com>
2024-05-21 20:44:14 +0900

    :bug: allauth수정

--- MyV/signup/admin.py
+++ MyV/signup/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+#@admin.register(User)
+'''class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'username', 'user_ID', 'user_mood', 'user_energy', 'user_tempo', 'is_admin', 'is_staff', 'is_superadmin', 'is_active')
     search_fields = ('email', 'username', 'user_ID')
     readonly_fields = ('date_joined', 'last_login')
 
     filter_horizontal = ()
     list_filter = ()
-    fieldsets = ()
+    fieldsets = ()'''
+    
+    
+admin.site.register(User, UserAdmin)
+
--- MyV/signup/models.py
+++ MyV/signup/models.py
@@ -1,80 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
-class UserManager(BaseUserManager):
-    def create_user(self, username, email, user_ID, user_mood, user_energy, user_tempo, password=None):
-        if not username:
-            raise ValueError('사용자는 무조건 이름이 있어야 합니다.')
-        
-        if not email:
-            raise ValueError('사용자는 무조건 이메일이 있어야 합니다.')
-        
-        if not user_ID:
-            raise ValueError('사용자는 무조건 ID가 있어야 합니다.')
 
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-            user_ID=user_ID,
-            user_mood=user_mood,
-            user_energy=user_energy,
-            user_tempo=user_tempo,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    
-    def create_superuser(self, username, email, user_ID, user_mood, user_energy, user_tempo, password=None):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            username=username,
-            user_ID=user_ID,
-            user_mood=user_mood,
-            user_energy=user_energy,
-            user_tempo=user_tempo,
-            password=password
-        )
-        
-        user.is_admin = True
-        user.is_active = True
-        user.is_staff = True
-        user.is_superadmin = True
-        user.save(using=self._db)
-        return user
+class User(AbstractUser):
+    pass
 
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=50)
-    email = models.EmailField(max_length=255, unique=True)
-    user_ID = models.CharField(max_length=50, unique=True)
-    user_mood = models.DecimalField(max_digits=4, decimal_places=2)
-    user_energy = models.DecimalField(max_digits=4, decimal_places=2)
-    user_tempo = models.DecimalField(max_digits=4, decimal_places=2)
-    
-    is_admin = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
-    
-    date_joined = models.DateTimeField(default=timezone.now)
-    last_login = models.DateTimeField(default=timezone.now) 
-    
-    USERNAME_FIELD = 'user_ID'
-    REQUIRED_FIELDS = ['username', 'email', 'user_mood', 'user_energy', 'user_tempo']
-    
-    objects = UserManager()
-
-    class Meta:
-        app_label = 'signup'
-    
-    def __str__(self):
-        return self.email
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, app_label):
-        return True
 
 class UploadMAXMIN(models.Model):
     max_file = models.FileField(upload_to='userVoice/')
