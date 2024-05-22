4c0e312207e7ecbab656de5141e7f17f29e1e95e
J1miin <gift040404@naver.com>
2024-05-23 02:10:57 +0900

    :card_file_box: 0523 모델에 사용자별 음역대 저장
    현재 마이그레이션 안됨
    back/signup 모델 머지 한 후 import 가능할 것으로 보임

--- MyV/modal/models.py
+++ MyV/modal/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from signup.models import User #Signup에서 만든 유저 모델을 FK로 사용하기 위해서 import 해주기
 
 class UploadAnalyzeFile(models.Model):
-    # 기존의 한 사용자로부터 인스턴스가 두 개씩 생기는 문제 방지 by setting user id as FK 
-    #user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     # AWS/vocalReportSource 디렉토리 버킷에 파일 업로드 
     mySound_file = models.FileField(upload_to='vocalReportSource/')
     compareSound_file = models.FileField(upload_to='vocalReportSource/')
     # 업로드한 파일명 저장
     fMySound_name = models.CharField(max_length=255, default='default')
     fCompareSound_name = models.CharField(max_length=255, default='default')
+
+#사용자 음역대 저장할 모델
+class UserVocalInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #signup으로 로그인한 유저의 보컬분석후 음역대 정보 저장하기
+    max_note = models.CharField(max_length=50, default = "none")
+    min_note = models.CharField(max_length=50, default = "none")
--- MyV/modal/views.py
+++ MyV/modal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from .models import UploadAnalyzeFile
+from .models import UploadAnalyzeFile, UserVocalInfo
 from .analyzing_file import process_file, downloadFile
 # Create your views here.
 
@@ -57,6 +57,9 @@ def vocalResult(request):
     if (downloaded==1):
         max_note, min_note, start_t, best_st, best_ed = process_file()
         context = {'max':max_note, 'min':min_note, 'start_t': start_t, 'best_st': int(best_st), 'best_ed':int(best_ed)}
+        user = request.user  # 현재 로그인한 사용자에 접근하기 위한 용도..
+        saveInfo = UserVocalInfo(user= user, max_note=max_note, min_note=min_note)
+        saveInfo.save()
     else :
         print("##파일이 다운로드 되지 않았음##")
     return render(request, 'modal/analyze_result.html', context)
