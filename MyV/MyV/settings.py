684a8f016cd84af867be057500f06a91c1025012
YeahOut <pt840072@gmail.com>
2024-05-22 19:08:03 +0900

    :lipstick: error메시지 출력

--- MyV/MyV/settings.py
+++ MyV/MyV/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'signup',
+    'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
