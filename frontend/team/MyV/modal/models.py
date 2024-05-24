f0443686643c8037f46f2b82ae497c29097f4e56
skckem <163977289+skckem@users.noreply.github.com>
2024-05-24 16:08:53 +0900

    team

--- frontend/team/MyV/modal/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class UploadAnalyzeFile(models.Model):
-    mySound_file = models.FileField(upload_to='vocalReportSource/')
-    compareSound_file = models.FileField(upload_to='vocalReportSource/')
--- frontend/team/MyV/modal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('analyze', views.analyzePage, name='analyze'),
-    path('analyze/upload', views.upload_analyze_file, name='upload_analyze_file'),
-    path('analyze/result', views.vocalResult, name='vocalResult'),
-    #path('analyze/+ing', views.vocalResult, name='vocalReport'),
-]
