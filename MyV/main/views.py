b962e4d4185417adcb9577a67e8a608bdac6cae5
J1miin <gift040404@naver.com>
2024-05-31 15:17:15 +0900

    :zap: 2번 이상의 노래추천 속도 빠르게 하기
     노래 추천 1번 한 이후는 리브로사 또 돌리지 않게 하기

--- MyV/main/views.py
+++ MyV/main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .awsInMain import upload_to_s3,downloadFile
 from .maxminAnalyze import maxminAnalyze
 from .sportify_api import sportify
+from .models import UserMaxMinNote
 
 ##async
 from asgiref.sync import sync_to_async
@@ -37,7 +38,10 @@ def main_2(request):
 
 def main_3(request):
     user = request.user
-    #maxminAnalyze(user)
+    #다시 추천 받을 때, 만약 최고음 최저음 분석을 한 번 했더라면 그냥 바로 디비 정보로 sportify만 돌리게하기 (리브로사 안쓰고!)
+    userNoteInfo = UserMaxMinNote.objects.filter(user=user).order_by('-id').first()  # 가장 마지막 정보만 가져오기
+    if userNoteInfo.min_note == False :
+        maxminAnalyze(user)
     #song_names~preview_urls는 각각 리스트
     #userInfo는 max,min, user_key_string, tmpo, energy 순으로 담긴 리스트
     song_names, song_urls, img_urls, preview_urls, artist, userInfo = sportify(user) 
