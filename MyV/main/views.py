ab8d42dfd33b399ae1ea277532d170f3c1c93c50
Jimin <88357100+J1miin@users.noreply.github.com>
2024-06-04 15:44:18 +0900

    Revert "Dev/jimin"

--- MyV/main/views.py
+++ MyV/main/views.py
@@ -3,15 +3,8 @@ from django.http import HttpResponse
 from .awsInMain import upload_to_s3,downloadFile
 from .maxminAnalyze import maxminAnalyze
 from .sportify_api import sportify
-from .models import UserMaxMinNote
 
-##async
-from asgiref.sync import sync_to_async
-import asyncio
-##
 
-def css(request):
-    return render(request,'main/main_3.html')
 def main_1(request):
     return render(request, 'main/main_1.html')
 
@@ -30,20 +23,14 @@ def upload_max_min(request):
     return HttpResponse("Failed to upload files")
 
 def main_2(request):
-    # user = request.user
-    # # task1 = asyncio.ensure_future(maxminAnalyze(user))
-    # # await asyncio.wait([task1])
-    # maxminAnalyze(user)
+    user = request.user
+    maxminAnalyze(user)
     return render(request,'main/loading.html')
 
 def main_3(request):
     user = request.user
-    #다시 추천 받을 때, 만약 최고음 최저음 분석을 한 번 했더라면 그냥 바로 디비 정보로 sportify만 돌리게하기 (리브로사 안쓰고!)
-    userNoteInfo = UserMaxMinNote.objects.filter(user=user).order_by('-id').first()  # 가장 마지막 정보만 가져오기
-    if userNoteInfo.min_note == False :
-        maxminAnalyze(user)
     #song_names~preview_urls는 각각 리스트
-    #userInfo는 max,min, user_key_string, tmpo, energy 순으로 담긴 리스트
+    #userInfo는 max,min, mood, tmpo, energy 순으로 담긴 리스트
     song_names, song_urls, img_urls, preview_urls, artist, userInfo = sportify(user) 
     context = {
         'user' : user,
